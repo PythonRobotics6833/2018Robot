098c45ad929cf985a34533d1f5e25e840ae08ca9
4168755 <hkerkhoff0563@student.phoenixunion.org>
2018-02-03 12:51:19 -0700

    -Added constructors for Drivetrain Java file

--- src/org/usfirst/frc/team6833/robot/Drivetrain.java
+++ src/org/usfirst/frc/team6833/robot/Drivetrain.java
@@ -1,5 +1,25 @@
 package org.usfirst.frc.team6833.robot;
 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
 public class Drivetrain {
-    
+    private DifferentialDrive myDrive;
+
+    public Drivetrain(Talon left, Talon right) {
+
+    }
+    public Drivetrain(int left_input, int right_input, double left_c, double right_c){
+        //left input is usually 1
+        Talon m1_left = new Talon(left_input);
+        SpeedControllerGroup m_left = new SpeedControllerGroup(m1_left);
+
+        //right input is usually 0
+        Talon m0_Right = new Talon(right_input);
+        SpeedControllerGroup m_right = new SpeedControllerGroup(m0_Right);
+        
+        myDrive= new DifferentialDrive(m_left, m_right);
+    }
+
 }
