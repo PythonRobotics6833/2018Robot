d36190c92c4cb39b0f50d3689084e3fc5b19ceae
4168755 <hkerkhoff0563@student.phoenixunion.org>
2018-02-16 16:43:59 -0700

    --Added all booleans to deturming whether our robot it turning, moving forward, or backward

--- src/org/usfirst/frc/team6833/robot/Autonomous.java
+++ src/org/usfirst/frc/team6833/robot/Autonomous.java
@@ -4,6 +4,14 @@ public class Autonomous {
 
     Drivetrain drive;
     int StartingPoint;
+    double turnL;
+    double turnR;
+
+    private boolean right;
+    private boolean left;
+    private boolean foward;
+    private boolean backward;
+
     public Autonomous(Drivetrain drive)
     {
         this.drive=drive;
@@ -14,6 +22,40 @@ public class Autonomous {
         this.drive=drive;
         this.StartingPoint=StartingPoint;
 
+    }
+    public void drive()
+    {
+        turnL=drive.getEncoderLeftV();
+        turnR=drive.getEncoderRightV();
+
+        if(turnL>0&&turnR<0)
+        {
+            right=true;
+            left=false;
+            foward=false;
+            backward=false;
+        }
+        else if(turnL<0&&turnR>0)
+        {
+            left=true;
+            right=false;
+            foward=false;
+            backward=false;
+        }
+        else if(turnL>0&&turnR>0)
+        {
+            left=false;
+            right=false;
+            foward=true;
+            backward=false;
+        }
+        else
+            {
+                left=false;
+                right=false;
+                foward=false;
+                backward=true;
+            }
     }
 
 
