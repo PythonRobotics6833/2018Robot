1e576f4dfeaf54f0897539dbaa567bc034027922
Harrison Kerkhoff <hkerkhoff0563@student.phoenixunion.org>
2018-02-17 08:49:18 -0700

    --I'd give you information about what I did but I'm too tired.

--- src/org/usfirst/frc/team6833/robot/Autonomous.java
+++ src/org/usfirst/frc/team6833/robot/Autonomous.java
@@ -11,99 +11,123 @@ public class Autonomous {
     private boolean left;
     private boolean foward;
     private boolean backward;
+    private double angle = 90;
 
     //all x lines in field
-    int fieldx1[]={ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx2[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx3[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx4[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx5[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx6[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx7[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx8[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx9[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx10[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx11[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx12[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx13[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx14[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx15[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx16[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx17[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
-    int fieldx18[]={0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int fieldx1[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx2[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx3[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx4[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx5[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx6[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx7[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx8[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx9[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx10[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx11[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx12[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx13[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx14[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx15[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx16[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx17[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    int fieldx18[] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-    int fieldy[][]=
+    int fieldy[][] =
             {
-            fieldx1,
-            fieldx2,
-            fieldx3,
-            fieldx4,
-            fieldx5,
-            fieldx6,
-            fieldx7,
-            fieldx8,
-            fieldx9,
-            fieldx10,
-            fieldx11,
-            fieldx12,
-            fieldx13,
-            fieldx14,
-            fieldx15,
-            fieldx16,
-            fieldx17,
-            fieldx18
+                    fieldx1,
+                    fieldx2,
+                    fieldx3,
+                    fieldx4,
+                    fieldx5,
+                    fieldx6,
+                    fieldx7,
+                    fieldx8,
+                    fieldx9,
+                    fieldx10,
+                    fieldx11,
+                    fieldx12,
+                    fieldx13,
+                    fieldx14,
+                    fieldx15,
+                    fieldx16,
+                    fieldx17,
+                    fieldx18
             };
 
 
-    public Autonomous(Drivetrain drive)
-    {
-        this.drive=drive;
+    public Autonomous(Drivetrain drive) {
+        this.drive = drive;
 
     }
-    public Autonomous(Drivetrain drive, int StartingPoint)
-    {
-        this.drive=drive;
-        this.StartingPoint=StartingPoint;
+
+    public Autonomous(Drivetrain drive, int StartingPoint) {
+        this.drive = drive;
+        this.StartingPoint = StartingPoint;
 
     }
-    public void driveActionCheck()
-    {
-        turnL=drive.getEncoderLeftV();
-        turnR=drive.getEncoderRightV();
 
-        if(turnL>0&&turnR<0)
-        {
-            right=true;
-            left=false;
-            foward=false;
-            backward=false;
-        }
-        else if(turnL<0&&turnR>0)
-        {
-            left=true;
-            right=false;
-            foward=false;
-            backward=false;
-        }
-        else if(turnL>0&&turnR>0)
-        {
-            left=false;
-            right=false;
-            foward=true;
-            backward=false;
-        }
-        else
-            {
-                left=false;
-                right=false;
-                foward=false;
-                backward=true;
+    public void calculatePosition(int StartingPoint, boolean blueTeam) {
+        if (blueTeam) {
+            switch (StartingPoint) {
+                case 0:
+                    fieldy[5][35] = 2;
+                    break;
+                case 1:
+                    fieldy[9][35] = 2;
+                    break;
+                case 2:
+                    fieldy[14][35] = 2;
+                    break;
             }
-    }
-    public void driveToWayPoint(int position)
-    {
-    
+        } else {
+            switch (StartingPoint) {
+                case 0:
+                    fieldy[5][0] = 2;
+                    break;
+                case 1:
+                    fieldy[9][0] = 2;
+                    break;
+                case 2:
+                    fieldy[14][0] = 2;
+                    break;
+            }
+        }
+
     }
 
+    public void driveActionCheck() {
+        turnL = drive.getEncoderLeftV();
+        turnR = drive.getEncoderRightV();
+
+        if (turnL > 0 && turnR < 0) {
+            right = true;
+            left = false;
+            foward = false;
+            backward = false;
+        } else if (turnL < 0 && turnR > 0) {
+            left = true;
+            right = false;
+            foward = false;
+            backward = false;
+        } else if (turnL > 0 && turnR > 0) {
+            left = false;
+            right = false;
+            foward = true;
+            backward = false;
+        } else {
+            left = false;
+            right = false;
+            foward = false;
+            backward = true;
+        }
+    }
+
+    public void driveToWayPoint(int position) {
+
+    }
 
 }
+
+
+
